d603d05a37b647cafc4a7ddc07bf92dd5d33c73a
Calvin <calvin@calvinmbp.local>
2017-02-13 16:36:18 -0800

    changes for image compression, forgot to commit earlier.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('minify', function() {
   return gulp.src('src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./'));
 });
-var cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify-css', function() {
-  return gulp.src('styles/*.css')
+  return gulp.src('src/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./css/'));
 });
-gulp.task('default', ['minify','minify-css']);
+
+gulp.task('minify-img', function() {
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/'))
+});
+gulp.task('minify-img-views', function() {
+    gulp.src('src/views/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('views/images/'))
+});
+
+gulp.task('default', ['minify','minify-css','minify-img']);
